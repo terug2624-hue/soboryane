c0022ae5a1495d605a74c4dd06c14e60c22b2530
terug2624-hue <terug2624@gmail.com>
2026-05-09 00:52:46 +0300

    Update sw.js old

--- sw.js
+++ sw.js
@@ -1,34 +1,4 @@
-const CACHE_NAME = 'soboryane-v100'; // Меняйте номер версии здесь при каждом обновлении
-const ASSETS = [
-  './',
-  './index.html',
-  './firebase-app.js',
-  './firebase-database.js',
-  './manifest.json',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-  // Пропускаем ожидание и сразу активируем новый SW
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  // Удаляем старые версии кэша автоматически
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+// Service Worker для приложения Соборяне v43
+self.addEventListener('fetch', function(event) {
+    // Этот код позволяет приложению работать быстрее
 });
