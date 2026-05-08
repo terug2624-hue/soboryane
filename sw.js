2d89c3b4263bf8f704228062de5b2edd6f31f84d
terug2624-hue <terug2624@gmail.com>
2026-05-08 23:04:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-// Service Worker для приложения Соборяне v43
-self.addEventListener('fetch', function(event) {
-    // Этот код позволяет приложению работать быстрее
+const CACHE_NAME = 'soboryane-v48';
+const ASSETS = [
+  './',
+  './index.html',
+  './firebase-app.js',
+  './firebase-database.js',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
+
