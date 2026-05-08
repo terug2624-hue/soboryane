5ee5d5ea18bbd5dd84fa8a89b53f9134630d0773
terug2624-hue <terug2624@gmail.com>
2026-05-09 00:30:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soboryane-v70'; // Меняйте номер версии здесь при каждом обновлении
+const CACHE_NAME = 'soboryane-v100'; // Меняйте номер версии здесь при каждом обновлении
 const ASSETS = [
   './',
   './index.html',
